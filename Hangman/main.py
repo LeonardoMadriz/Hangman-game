53cb203696dc1569974a32057b8f10b627d530f0
Leonardo Madriz <20-10399@usb.ve>
2023-06-02 00:10:55 -0400

    lectura finalizada

--- Hangman/main.py
+++ Hangman/main.py
@@ -1,3 +1,16 @@
+import random
+
+def read():
+    list=[]
+    with open("../DataBase/data.txt","r",encoding="utf-8") as f:
+        for line in f:
+            list.append(line)
+    return list[random.randint(1,170)]
+
+
+
+
+
 def run():
     pass
 
